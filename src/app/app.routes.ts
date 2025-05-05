73eb470aed5dd0551f49692386f3e54c860e66dc
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-05 15:10:50 +0200

    routes och httpclient i config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CourseListComponent } from './components/course-list/course-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'courselist', component: CourseListComponent},
+    { path: '', redirectTo: 'courselist', pathMatch: 'full' } 
+];

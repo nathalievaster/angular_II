9f230a897a3cb685aeadf198072bb168a1e542c3
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-05 15:01:09 +0200

    hämtar kursdata via CourseService

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Courses } from '../models/courses';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  private url: string = "https://webbutveckling.miun.se/files/ramschema.json";
+
+  http = inject(HttpClient);
+
+  getCourses(): Observable<Courses[]> {
+    return this.http.get<Courses[]>(this.url);
+  }
 }
